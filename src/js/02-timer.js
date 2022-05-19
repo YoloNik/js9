a6981d022f210d595e6f6305f572041f1909ee28
YoloNik <nikitaslipachuk@gmail.com>
2022-05-19 07:28:50 +0300

    test1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const refs = {
   inputDatetime: document.querySelector(`#datetime-picker`),
@@ -18,13 +19,18 @@ const fp = flatpickr(refs.inputDatetime, {
 
   onClose(selectedDates) {
     if (selectedDates[0] < this.now) {
-      window.alert('Please choose a date in the future');
+      Notiflix.Notify.failure('Please choose a date in the future');
       return;
     }
     refs.startBtn.disabled = false;
     console.log(selectedDates[0].getTime());
   },
 });
+
+let currentDates = Date.now();
+let timeLeftConvert;
+
+refs.startBtn.addEventListener(`click`, start);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -45,11 +51,9 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-let currentDates = Date.now();
-let timeLeftConvert;
-
-refs.startBtn.addEventListener(`click`, start);
-
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 function start(e) {
   refs.startBtn.disabled = true;
   let timeLeftUNIX = fp.selectedDates[0].getTime() - currentDates;
@@ -58,25 +62,26 @@ function start(e) {
     timeLeftConvert = convertMs(timeLeftUNIX);
 
     refs.timer.innerHTML = `<div class="field">
-        <span class="value" data-days>${timeLeftConvert.days}</span>
+        <span class="value" data-days>${addLeadingZero(timeLeftConvert.days)}</span>
         <span class="label">Days</span>
       </div>
       <div class="field">
-        <span class="value" data-hours>${timeLeftConvert.hours}</span>
+        <span class="value" data-hours>${addLeadingZero(timeLeftConvert.hours)}</span>
         <span class="label">Hours</span>
       </div>
       <div class="field">
-        <span class="value" data-minutes>${timeLeftConvert.minutes}</span>
+        <span class="value" data-minutes>${addLeadingZero(timeLeftConvert.minutes)}</span>
         <span class="label">Minutes</span>
       </div>
       <div class="field">
-        <span class="value" data-seconds>${timeLeftConvert.seconds}</span>
+        <span class="value" data-seconds>${addLeadingZero(timeLeftConvert.seconds)}</span>
         <span class="label">Seconds</span>
       </div>`;
 
     if (Math.floor(timeLeftUNIX / 1000) === 0) {
+      Notiflix.Notify.success('Time is up!');
       clearInterval(timerId);
       refs.startBtn.disabled = false;
     }
-  });
+  }, 1000);
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs = {
+  delay: document.querySelector(`input[name=delay]`),
+  step: document.querySelector(`input[name="step"]`),
+  amount: document.querySelector(`input[name="amount"]`),
+  button: document.querySelector(`button`),
+};
+
+refs.button.addEventListener(`click`, onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  let firstDelay = refs.delay.value;
+  let delayStep = refs.step.value;
+  let amount = refs.amount.value;
+  let counterOfAmount = 0;
+  let counterOfSteps = 0;
+
+  createPromise().finally(Notiflix.Notify.warning(`☝️Your request is being processed`));
+
+  const intervalId = setInterval(() => {
+    createPromise(amount, delayStep)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${counterOfAmount} in ${counterOfSteps}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${counterOfAmount} in ${counterOfSteps}ms`);
+      });
+    counterOfAmount += 1;
+    counterOfSteps += +delayStep;
+    if (counterOfAmount === +amount) {
+      clearInterval(intervalId);
+    }
+  }, delayStep);
+
+  function createPromise(position, delay) {
+    return new Promise((fulfilled, rejected) => {
+      const shouldResolve = Math.random() > 0.3;
+      setTimeout(() => {
+        if (shouldResolve) {
+          fulfilled({ position, delay });
+        } else {
+          rejected({ position, delay });
+        }
+      }, firstDelay);
+    });
   }
 }
